c62d267056f863fcd488d06cb9f9189eaed8d8f6
sakshi <sakshi.panchale@dorset.ie>
2025-10-07 14:09:38 +0100

    Movie Driver

---
+++ oop-20251007-a-drivers-files-strings/Models/AppData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_20251007_a_drivers_files_strings.Models
+{
+    public static class AppData
+    {
+        public const string AppName = "oop-20251007-a-drivers-files-strings";
+        public const string AppVersion = "1.0.0";
+        public const string AppAuthor = "John Rowley";
+    }
+}
--- oop-20251007-a-drivers-files-strings/Program.cs
+++ oop-20251007-a-drivers-files-strings/Program.cs
@@ -1,3 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Mo
+using oop_20251007_a_drivers_files_strings.Models;
+using oop_20251007_a_drivers_files_strings.RunDemos;
+
+AppDriver.Run();
+FileDriver.Run();
+MovieDriver.Run();
+StringDriver.Run();
+
+
+        
